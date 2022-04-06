9a1523080cc38b88990c4ffb5c1de5146a2ac60e
khufkens <koen.hufkens@gmail.com>
2022-04-06 14:01:00 +0200

    validation runs

--- benchmarking/00_benchmark_fluxnet_data.R
+++ benchmarking/00_benchmark_fluxnet_data.R
@@ -9,106 +9,102 @@ library(rsofun)
 library(dplyr)
 library(readr)
 library(ingestr)
+source("R/eval_sofun.R")
+source("R/get_stats.R")
 
 # read in benchmarking driver data -----
-load("data/p_model_fluxnet_drivers.rda")
-load("data/p_model_fluxnet_calval.rda")
+load("data/df_drivers_fluxnet2015_allsites.rda")
+load("data/obs_eval_fluxnet2015.rda")
+flue_sites <- readr::read_csv( "data/flue_stocker18nphyt.csv" ) %>%
+  dplyr::filter( !is.na(cluster) ) %>%
+  distinct(site) %>%
+  pull(site)
 
-# fix the tmin / tmax issue for now ----
-# p_model_fluxnet_drivers <- p_model_fluxnet_drivers %>%
-#   mutate(forcing = purrr::map(forcing,
-#                            ~mutate(., tmin = temp)),
-#          forcing = purrr::map(forcing,
-#                            ~mutate(., tmax = temp)))
-#
-# save(p_model_fluxnet_drivers,
-#      file = "data/p_model_fluxnet_drivers.rda",
-#      compress = "xz")
+calibsites <- ingestr::siteinfo_fluxnet2015 %>%
+  dplyr::filter(!(sitename %in% c("DE-Akm", "IT-Ro1"))) %>%
+  dplyr::filter(sitename != "FI-Sod") %>%
+  dplyr::filter( c4 %in% c(FALSE, NA) & classid != "CRO" & classid != "WET" ) %>%
+  dplyr::filter( sitename %in% flue_sites ) %>%
+  pull(sitename)
 
-# set calibration settings ----
-settings_calib <- list(
-  method              = "gensa",
-  targetvars          = c("gpp"),
-  timescale           = list( gpp = "d" ),
-  maxit               = 5,
-  sitenames           = p_model_fluxnet_calval$sitename,
-  metric              = cost_rmse_kphio,
-  dir_results         = "./",
-  name                = "FULL",
-  par                 = list(
-    kphio       = list( lower=0.03, upper=0.1, init= 0.05 ),
-    soilm_par_a = list( lower=0.0,  upper=1.0, init=0.0 ),
-    soilm_par_b = list( lower=0.0,  upper=1.5, init=0.6 ) )
- )
-
-# calibrate model parameters ----
-set.seed(1982)
-
-drivers <- p_model_fluxnet_drivers %>%
-  dplyr::filter(sitename %in% p_model_fluxnet_calval$sitename)
-
-estimated_parameters <- calib_sofun(
-  df_drivers = drivers,
-  ddf_obs = p_model_fluxnet_calval,
-  settings = settings_calib
+drivers <- df_drivers_fluxnet2015_allsites %>%
+  filter(
+    sitename %in% flue_sites
+  ) %>%
+  filter(
+    sitename %in% calibsites
+  ) %>%
+  rename(
+    "site_info" = "siteinfo",
+    "params_soil" = "df_soiltexture"
   )
 
-# print the best parameters ----
-print(estimated_parameters$par_opt)
-save(estimated_parameters,
-     file = "data/estimated_parameters.rda",
-     compress = "xz")
+# calibrate model parameters ----
+# set.seed(1982)
+# settings <- list(
+#   method              = "gensa",
+#   targets             = c("gpp"),
+#   sitenames           = calibsites,
+#   metric              = rsofun::cost_rmse_fullstack,
+#   control = list(
+#     sampler = "DEzs",
+#     settings = list(
+#       nrChains = 1,
+#       burnin = 1,
+#       iterations = 4
+#     )
+#   ),
+#   par = list(
+#     a = list(lower=0.04, upper=0.09, init=0.05),
+#     b = list(lower=0.5, upper=5, init=3.5),
+#     c = list(lower=2, upper=5, init=3.5)
+#   )
+# )
+#
+# drivers <- p_model_fluxnet_drivers %>%
+#   dplyr::filter(sitename %in% calibsites)
+#
+# estimated_parameters <- rsofun::calib_sofun(
+#   drivers = drivers,
+#   obs = p_model_fluxnet_calval,
+#   settings = settings
+#   )
 
-# # update default model parameters ----
-# params_modl <- list(
-# 	kphio           = 0.05,
-# 	soilm_par_a     = 1.0,
-# 	soilm_par_b     = 0.0,
-#   tau_acclim_tempstress = 10,
-#   par_shape_tempstress  = 0.0
-# 	)
-# params_modl <- update_params(params_modl, settings_calib)
-#
-# # run the model with the paramters ----
-# df_output <- runread_pmodel_f(
-#      df_drivers_fluxnet2015,
-#      params_modl = params_modl,
-#      makecheck = TRUE,
-#      parallel = FALSE
-#      )
-#
-# save(df_output, file = "~/data/rsofun_benchmarking/df_output_v3.4.2.Rdata")
-#
-# evalsites <- df_output %>%
-#   mutate(ntsteps = purrr::map_dbl(data, ~nrow(.))) %>%
-#   dplyr::filter(ntsteps > 0) %>%
-#   pull(sitename)
-#
-#
-#
-# settings_eval <- list(
-#   benchmark = list( gpp = c("fluxnet") ),
-#   sitenames = evalsites,
-#   agg       = 8  # An integer specifying the number of days used to
-#             #define the width of bins for daily data aggregated to several days
-#   )
-#
-# out_eval <- eval_sofun(
-#   df_output,
-#   settings_eval,
-#   settings_sims,
-#   obs_eval = obs_eval,
-#   overwrite = TRUE,
-#   light = FALSE
-#   )
-#
-# out_eval$gpp$fluxnet$plot$gg_modobs_xdaily
-# out_eval$gpp$fluxnet$plot$gg_modobs_spatial_annual
-#
-#
-# df_output_allsites <- runread_pmodel_f(
-#      df_drivers_fluxnet2015,
-#      params_modl = params_modl,
-#      makecheck = TRUE,
-#      parallel = FALSE
-#      )
+params_modl <- list(
+  kphio           = 0.09423773, # 0.08791485, # pars$par[1],
+  soilm_par_a     = 0.33349283, # 0.59107163, # pars$par[2],
+  soilm_par_b     = 1.45602286, # pars$par[3],
+  tau_acclim_tempstress = 10,   # irrelevant - not calibrated
+  par_shape_tempstress  = 0.0   # irrelevant - not calibrated
+)
+
+# run the model with the paramters ----
+output <- runread_pmodel_f(
+     drivers,
+     par = params_modl,
+     makecheck = TRUE,
+     parallel = FALSE
+     )
+
+evalsites <- output %>%
+  mutate(ntsteps = purrr::map_dbl(data, ~nrow(.))) %>%
+  dplyr::filter(ntsteps > 0) %>%
+  pull(sitename)
+
+settings_eval <- list(
+  benchmark = list( gpp = c("fluxnet") ),
+  sitenames = evalsites,
+  agg       = 8  # An integer specifying the number of days used to
+            #define the width of bins for daily data aggregated to several days
+  )
+
+out_eval <- eval_sofun(
+  output,
+  settings_eval,
+  obs_eval = obs_eval,
+  overwrite = TRUE,
+  light = FALSE
+  )
+
+out_eval$gpp$fluxnet$plot$gg_modobs_xdaily
+out_eval$gpp$fluxnet$plot$gg_modobs_spatial_annual
